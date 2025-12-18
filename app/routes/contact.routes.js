eedf71f2850e9b69d0658d020f1adcdc99f81acf
Team-HV01 <manishdeveloper6@gmail.com>
2025-12-18 23:02:20 +0530

    Delete app directory

--- app/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-const { verifyAdminToken } = require("../middleware/auth.js");
-
-module.exports = (router) => {
-    const cartController = require("../controller/contact.controller.js");
-
-    router.get("/support", verifyAdminToken, cartController.getAllDetails);
-    router.post('/user/support', cartController.submitForm);
-};
